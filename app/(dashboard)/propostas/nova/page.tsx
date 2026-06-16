d1a99e3f3d4db4b4645d6bccdd8c41f9ac9b4158
gestao-hypebit <gestao@hypebit.com.br>
2026-06-16 10:35:45 -0300

    updates

--- app/(dashboard)/propostas/nova/page.tsx
+++ app/(dashboard)/propostas/nova/page.tsx
@@ -138,7 +138,7 @@ export default function NovaPropostaPage() {
           </h1>
         </div>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <div className="lg:col-span-2 space-y-6">
               <Card>
